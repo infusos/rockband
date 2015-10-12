9968ca72a7bcb0ca7204031d879c5971704b5d32
Dani Santeugini <hefesto84@gmail.com>
2015-10-12 18:01:13 +0200

    First commit

---
+++ Assets/Scripts/Core/BaseApplication.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseApplication : MonoBehaviour {
+
+	private EventsManager mEventsManager;
+
+	void Start () {
+		GameObject.DontDestroyOnLoad (this);
+		mEventsManager = new EventsManager ();
+	}
+
+	void Update () {
+	
+	}
+
+}
---
+++ Assets/Scripts/Manager/EventsManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventsManager{
+
+	public delegate void LoadScene(string test);
+
+	public event LoadScene OnLoadScene;
+
+	public EventsManager(){
+
+	}
+
+}
